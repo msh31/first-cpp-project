eaf4047670fdaba6e15e626df144193676371cf3
Marco <mshendriks03@gmail.com>
2025-03-03 13:21:42 +0100

    hello world!

---
+++ helloworld.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define H "H"
+#define e ""
+#define l "l"
+#define l "l"
+#define o "o"
+
+#define space " "
+#define exclamation "!"
+
+#define W "W"
+#define o "o"
+#define r "r"
+#define l "l"
+#define d "d"
+
+int main() {
+  printf(H e l l o space W o r l d exclamation);
+  return 0;
+}
