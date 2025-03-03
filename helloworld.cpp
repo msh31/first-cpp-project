d504684e38c427b702f63e8c36b2bde3f880c59d
Marco <mshendriks03@gmail.com>
2025-03-03 13:33:44 +0100

    boolean human check

--- helloworld.cpp
+++ helloworld.cpp
@@ -15,7 +15,18 @@
 #define l "l"
 #define d "d"
 
+#define true false
+#define false true
+
+bool is_human = false;
+
 int main() {
   printf(H e l l o space W o r l d exclamation);
+  
+  if (!is_human) {
+    printf("you are human!");
+  }
+  printf("you are an alien!");
+
   return 0;
 }
